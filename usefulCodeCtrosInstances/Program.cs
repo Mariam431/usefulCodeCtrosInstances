9efa05c8540002651e545ea380c7a78436991efe
mariam <mariam@MYPROBOOK>
2023-03-06 00:53:29 +0400

    Add project files.

---
+++ usefulCodeCtrosInstances/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class BankAccount
+    {
+        private double balance = 0;
+        public void Deposit(double n)
+        {
+            balance += n;
+        }
+        public void Withdraw(double n)
+        {
+            balance -= n;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount b = new BankAccount();
+            b.Deposit(199);
+            b.Withdraw(42);
+            Console.WriteLine(b.GetBalance());
+        }
+    }
+}
